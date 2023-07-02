ca3a6900499ccd88cb866ab0fe0cda85c2ea896d
R11G <richlgame31@gmail.com>
2023-07-02 16:31:28 -0400

    Table test

--- clicker/script.js
+++ clicker/script.js
@@ -1,10 +1,33 @@
 let sl = 0;
 let ge = 0;
 let debt = 0;
+let units = [];
 function baseBombed() {
   sl += 1000;
 }
 function creditCard() {
   ge += 100;
   debt++;
+}
+function readFile(file) {
+  let reader = new FileReader();
+  reader.onload = (e) => {
+    const file = e.target.result;
+    const lines = file.split(/\r\n|\n/);
+    for (var i = 0; i < lines.length; i++) {
+      units[i] = lines[i].split(";");
+    }
+  }
+  reader.readAsText(file);
+}
+function displayTable() {
+  let table = document.createElement('table');
+  for (let row of units) {
+    table.insertRow();
+    for (let cell of row) {
+      let newCell = table.rows[table.rows.length - 1].insertCell();
+      newCell.textContent = cell;
+    }
+  }
+  document.body.appendChild(table);
 }
