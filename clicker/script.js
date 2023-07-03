c7426634e6efe17a3355c6d201b0776085c8690e
R11G <richlgame31@gmail.com>
2023-07-03 16:44:17 -0400

    BRs and SL/GE icons

--- clicker/script.js
+++ clicker/script.js
@@ -12,7 +12,6 @@ function creditCard() {
   debt++;
 }
 function generateTable() {
-  // creates a <table> element and a <tbody> element
   const tbl = document.createElement("table");
   const tblBody = document.createElement("tbody");
   for (let i = 0; i < units.length; i++) {
@@ -29,14 +28,26 @@ function generateTable() {
           cell.appendChild(cellInfo);
           const cellInfo2 = document.createElement("img");
           cellInfo2.src = "clicker/28px-Ge_icon.png";
-          cell.appendChild(cellInfo);
+          cell.appendChild(cellInfo2);
         } else {
           const cellInfo = document.createTextNode(units[i][j].substring(0,units[i][j].length - 2));
           cell.appendChild(cellInfo);
+
           const cellInfo2 = document.createElement("img");
           cellInfo2.src = "clicker/28px-Sl_icon.png";
           cell.appendChild(cellInfo2);
         }
+      } else if (j==9) {
+        let br = parseInt(units[i][j]);
+        let mod = br%3;
+        br = Math.floor(br/3) * 1 + 1;
+        if (mod == 1) {
+          br += .3;
+        } else if (mod == 2) {
+          br += .7;
+        }
+        const cellInfo = document.createTextNode(br);
+        cell.appendChild(cellInfo);
       } else {
         const cellInfo = document.createTextNode(units[i][j]);
         cell.appendChild(cellInfo);
